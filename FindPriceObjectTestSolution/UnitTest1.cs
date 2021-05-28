d6d69422398650156ef14609bcdb0ef00058e90f
antony.kidis <antony.kidis@gmail.com>
2021-05-28 16:57:09 +0300

    Add project files.

---
+++ FindPriceObjectTestSolution/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+namespace FindPriceObjectTestSolution
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //global Class member variable
+        bool _IsDressFound = false;
+        bool _IsPriceCorrect = false;
+        bool _isButtonPressed = false;
+
+
+        /// <summary>
+        /// 1.Connects to a website.
+        /// 2.Locates Products
+        /// 3.Finds Black Blouse(from products)
+        /// 4.Verifies the blouse' price
+        /// </summary>
+        
+        [TestMethod]
+        public void SelectBlouse_Black_OnPrice()
+        {
+            IWebDriver driver = new ChromeDriver();                                         //Define IWebDriver driver-object
+            driver.Url = "http://automationpractice.com";                                   //Setup URL, and store in driver.Url
+            IWebElement CollectionOfULElements = driver.FindElement(By.Id("homefeatured")); //Select UL element that contains nested <li></li> elements
+
+            //Find all elements with a class-name of = "ajax_block_product" inside CollectionOfULElements
+            ReadOnlyCollection<IWebElement> dresses = CollectionOfULElements.FindElements(By.ClassName("ajax_block_product"));
+
+            //Once we a have a collection of all <li> elements
+            //We can now search for a black dress
+            for (int i = 0; i < dresses.Count; i++)
+            {
+                //Search for a class named: "product-name" in each <li> element inside dresses collection
+                //And check wether dresses[i] element are equal to  a search criteria("Blouse")
+                IWebElement name = dresses[i].FindElement(By.ClassName("product-name"));
+
+                if (name.Text == "Blouse")
+                {
+                    _IsDressFound = true;
+                    //A match found
+                    //Find <span> element that holds the price of "Blouse"
+                    IWebElement Price = dresses[i].FindElement(By.ClassName("price"));
+
+                    //Extract text from <span> element
+                    string PriceText = Price.GetAttribute("innerText");
+
+                    //Remove \t,\r\n elements from a string
+                    string PriceText_Clear = Regex.Replace(PriceText, @"\t|\n|\r", "");
+
+                    //Check the price (Throws expetion in case of fail)
+                    Assert.AreEqual("$27.00", PriceText_Clear, "Expected price are not equal to actual price");
+                    _IsPriceCorrect = true;
+
+                    //Locate the [Blouse] "add to cart" button
+                    IWebElement AddToCartBtn = dresses[i].FindElement(By.ClassName("ajax_add_to_cart_button"));
+
+                    //press the button
+                    AddToCartBtn.Click();
+                    _isButtonPressed = true;
+
+                    //exit for loop
+                    break;
+                }
+
+            } //end for...
+
+            //Final Test examination If all true the test  passes, otherwise test fails
+            Assert.IsTrue(_IsDressFound, "Dress Element is not found");
+            Assert.IsTrue(_IsPriceCorrect, "Price(string) actual, and expected results are not equal");
+            Assert.IsTrue(_isButtonPressed, "'Add_to_cart' button element is not found");
+
+
+        }
+    }
+}
---
+++ UnitTestProject1/FirstLesson.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+namespace test1021
+{
+    [TestClass]
+    public class FirstLesson
+    {
+      
+
+        //Exercise #1
+        //1 Open website
+        [TestMethod]
+        public void LoginTest()
+        {
+            //test login button
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://automationpractice.com";
+            IWebElement signIn = driver.FindElement(By.ClassName("login"));
+            signIn.Click();
+
+
+            //test Email
+            IWebElement Email = driver.FindElement(By.Id("email"));
+            Email.SendKeys("Antony.kidis1@gmail.com");
+
+            //Test Password
+            IWebElement Password = driver.FindElement(By.Id("passwd"));
+            Password.SendKeys("Automation@123");
+
+            //Click Sign-In button
+            IWebElement SubmitButton = driver.FindElement(By.Id("SubmitLogin"));
+            SubmitButton.Click();
+
+
+            //test Name-Autentication
+            IWebElement AutenticationDiv = driver.FindElement(By.Id("center_column"));
+            AutenticationDiv.FindElement(By.ClassName("page-heading"));
+
+            Assert.AreEqual(AutenticationDiv.Text, "MY ACCOUNT", "IsNot matched");
+        }
+
+
+        //Registration 1
+        [TestMethod]
+        public void FirstSelenium()
+        {
+            //test login button
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://automationpractice.com";
+            IWebElement signIn = driver.FindElement(By.ClassName("login"));
+            signIn.Click();
+
+            //test Email
+            IWebElement Email = driver.FindElement(By.Id("email_create"));
+            Email.SendKeys("ZhuiHui@gmail.com");
+
+            //test Create
+            IWebElement CreasteBtn = driver.FindElement(By.Id("SubmitCreate"));
+            CreasteBtn.Click();
+        }
+
+        //Registration-2
+        [TestMethod]
+        public void Registration()
+        {
+            //drive as
+            IWebDriver driver = new ChromeDriver();
+           
+            //Your Personal Information.............................
+            //Select Gender Mister
+            IWebElement SelectGender_Mr = driver.FindElement(By.Id("id_gender1"));
+            SelectGender_Mr.Click();
+
+            //Select Gender Missis
+            IWebElement SelectGender_Mrs = driver.FindElement(By.Id("id_gender1"));
+            SelectGender_Mrs.Click();
+
+            //Enter FirsName
+            IWebElement FirFirstName = driver.FindElement(By.Id("customer_firstname"));
+            FirFirstName.SendKeys("John");
+
+            //Enter FirsName
+            IWebElement LastName = driver.FindElement(By.Id("customer_lastname"));
+            FirFirstName.SendKeys("Travolta");
+
+            //test Email
+            IWebElement Email = driver.FindElement(By.Id("email_create"));
+            Email.Equals("ZhuiHui@gmail.com");
+
+            //Password
+            IWebElement Password = driver.FindElement(By.Id("passwd"));
+            Password.SendKeys("Pass@123");
+
+
+
+
+
+        }
+
+        [TestMethod] //notation the below function must be public
+        public void TestSum()
+        {
+            //send 2+2 to Sum, and verify the result is 4
+            int result = Sum(2, 2);
+            Assert.AreEqual(4, result); //we put assert in the end of the test!!!
+            Assert.AreEqual(4, result,"result is not 4"); //expected is 4
+        }
+
+        //test#2
+        [TestMethod] //notation the below function must be public
+        public void TestSum1()
+        {
+            Console.WriteLine("start test");
+            //send 2+2 to Sum, and verify the result is 4
+            int result = Sum(3, 2);
+            Console.WriteLine("end test");
+            //Assert.AreEqual(4, result);
+            Assert.AreEqual(4, result, "result is not 4");
+        }
+
+
+        //Methods..................................
+        public int Sum(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+
+        }
+
+        public int Seven()
+        {
+            return 7;
+        }
+
+        public string FullName(string FirstName, string LastName)
+        {
+            //Must be readable...
+            string Fullname = "Hello" + FirstName + " " + LastName;
+            return Fullname;
+        }
+
+        public int Sum(int num1, int num2, int num3)
+        {
+            int sum = num1 + num2 + num3;
+            return sum;
+        }
+            
+    }
+}
